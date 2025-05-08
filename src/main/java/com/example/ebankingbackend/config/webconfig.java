e91c0aa312250483f90985af587c9348d3625d33
rima-gif <achourryma971@gmail.com>
2025-05-08 18:38:03 +0200

    aa

--- src/main/java/com/example/ebankingbackend/config/webconfig.java
+++ src/main/java/com/example/ebankingbackend/config/webconfig.java
@@ -15,7 +15,7 @@ public class webconfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://192.168.40.112:30080")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                                 .allowedHeaders("*")
                                 .allowCredentials(true);
